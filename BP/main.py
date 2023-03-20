eaec9801278157d95d5a9344c91b61ff0973f8ff
MikkoBra <m.brandon.1@student.rug.nl>
2023-03-20 22:45:02 +0100

    Project skeleton

---
+++ BP/main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import scipy
+import pandas
+
+def print_hi():
+    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+    names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+    data = pandas.read_csv(url, names=names)
+    print(data.shape)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
